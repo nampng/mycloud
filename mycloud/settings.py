2eb9b742be70d7e8ca6f62da63a4968733376fd2
Nam Nguyen <nampn344@gmail.com>
2021-07-08 12:49:12 -0500

    update

--- mycloud/settings.py
+++ mycloud/settings.py
@@ -76,8 +76,9 @@ WSGI_APPLICATION = 'mycloud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db.mycloud',
+        'USER': 'root',
     }
 }
 
