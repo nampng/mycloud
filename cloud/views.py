a64b9715da2b8f8b6181cac8c4730abedb8ba170
Nam Nguyen <nampn344@gmail.com>
2021-07-09 15:38:30 -0500

    update

--- cloud/views.py
+++ cloud/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from cloud.models import FileModel
+import os
+from django.http import HttpResponse, Http404
 
 # Create your views here.
 
 def index(response):
     file_list = FileModel.objects.all()
     total_files = len(file_list)
-    return render(response, 'cloud/index.html', {'total_files':total_files, 'file_list':file_list})
+    recent_files = []
+    if file_list:
+        recent_files = file_list.order_by('-id')[:5]
+        for file in recent_files:
+            print(f"Recent file: {file.name}")
+    return render(response, 'cloud/index.html', {'total_files':total_files, 'recent_files':recent_files})
 
 def files(response):
 
@@ -20,6 +27,7 @@ def files(response):
                 print(response.FILES)
                 print("There's a file!")
                 uploaded_file = response.FILES['uploaded_file']
+                uploaded_file.name = uploaded_file.name.replace(' ', '_')
                 print(f"File name: {uploaded_file.name}, File size: {uploaded_file.size}")
                 new_file = FileModel(name=uploaded_file.name, file=uploaded_file)
                 new_file.save()
@@ -29,6 +37,11 @@ def files(response):
         if response.POST.get("download_file"):
             print("Downloading file")
             file_id = response.POST["download_file"][3:]
+            file = FileModel.objects.get(id=file_id)
+            with open(file.name, 'rb') as f:
+                http_response = HttpResponse(f.read())
+                http_response['Content-Disposition'] = 'attachment; filename= ' + file.name
+                return http_response
 
         if response.POST.get("delete_file"):
             print("Deleting file")
